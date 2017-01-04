9d7d309aa49bc00f745244f7a7a94f7bc6715291
ya-linghuang <103051060@live.asia.edu.tw>
2017-01-04 10:58:48 +0800

    Update hw01_103051060.java

--- src/hw/hw01_103051060.java
+++ src/hw/hw01_103051060.java
@@ -3,11 +3,39 @@ package hw;
 * Date: 2016/12/26
 * Author: 103051060 黃雅鈴
 */
+import java.util.Scanner;
 public class hw01_103051060 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	Scanner scn = new Scanner(System.in);
+		while(true){
+		int a =scn.nextInt();
+		int go [] =new int [a];
+		int back[] =new int[a];
+		for(int i =0 ; i<a ; i++){
+			go[i]=scn.nextInt();
+			back[i]=scn.nextInt();
+		}
+		for(int i =0 ; i<a ;i++){
+			if(go[i]>24||go[i]<=0){
+				System.out.print("Input Error!");
+				break;
+				}
+			if(back[i]>24||back[i]<=0){
+				System.out.print("Input Error!");
+				break;
+				}
+			for(int j = 0 ; j<a ; j++){
+				if(i==j){continue;}
+				if(back[i]==go[j]){
+					a-=1;
+				}
+			}
+		}
+		System.out.println(a);
+		break;
+		}
 	}
 
 }
